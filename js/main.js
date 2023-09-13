f6bdad852a1e8cdf3315cb48ad373272663950c2
wersomd <ileskazybek@gmail.com>
2023-09-14 01:04:24 +0600

    First

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const burgerMenu = document.getElementById("burger");
+const navbarMenu = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("is-active");
+  navbarMenu.classList.toggle("is-active");
+
+  if (navbarMenu.classList.contains("is-active")) {
+    navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+  } else {
+    navbarMenu.removeAttribute("style");
+  }
+});
+
+const menuLinks = document.querySelectorAll(".menu-link[data-goto]");
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        120 -
+        document.querySelector("header").offsetHeight;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+
+      e.preventDefault();
+    }
+  }
+}
